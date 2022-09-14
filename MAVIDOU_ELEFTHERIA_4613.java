e4bcc5b294413f48796421f4ad93215ca026acc6
elevmavi <83909565+elevmavi@users.noreply.github.com>
2022-09-14 16:49:17 +0300

    Add files via upload

---
+++ MAVIDOU_ELEFTHERIA_4613.java
@@ -0,0 +1,24 @@
+//MAVIDOU ELEFTHERIA 4613
+
+public class Product
+{
+	
+	List<Array> breakupString(String product) throws Exception
+	{
+		
+		product = "GR-12AEW-433A";
+		
+		@Test
+		List<Array> result = product(Arrays.asList(1,2));
+        assertThat(result,is(Arrays.asList(1)));
+		
+		@Test
+		List<Integer> result = Foo.dooFoo(Arrays.asList(3,4,5,6,7));
+		assertThat(result,is(Arrays.asList(2)));
+		
+		@Test
+		List<Integer> result = Foo.dooFoo(Arrays.asList(8,9,10,11));
+		assertThat(result,is(Arrays.asList(3)));
+	}
+
+}
